31926801830f4a236c521e69f61dd17e0beabd64
ForGive <zhaoxinghao5512@163.com>
2022-04-09 19:41:14 +0800

    Create 数据类型.py

---
+++ 数据类型.py
@@ -0,0 +1,12 @@
+# Python中有两种数据，数字和字符串
+# 本文先讲整型(int)，浮点数(float),字符串(str)，布尔型(bool).
+# Python中可以通过type()命令来获得一个数据的类型，例如：
+print(type(1))
+# 将会输出"<class 'int'>"
+"""
+class后面的引号部分即为此数据的数据类型，int整型无最大大小，它是整数类型。
+如果想要了解系统可承载的最大int值，可通过sys.maxsize得到系统承载最大int值
+float浮点型的最大大小为float("inf")
+
+"""
+
